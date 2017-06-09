7f165d5340ff97c7b5265978b8b6c5ef9564944b
Dominik Guzei <dominik.guzei@gmail.com>
2017-06-09 12:13:55 +0100

    fixes storybook webpack config

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,20 +1,33 @@
+const findCacheDir = require('find-cache-dir');
+
 module.exports = {
   module: {
     loaders: [
       {
         test: /\.(?:png|svg)$/,
-        loader: 'url',
+        loader: 'url-loader',
       },
       {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: [/(node_modules)/, /react-css-themr/]
+        test: /\.jsx?$/,
+        exclude: [/(node_modules)/, /react-css-themr/],
+        loader: require.resolve('babel-loader'),
+        query: {
+          babelrc: false,
+          cacheDirectory: findCacheDir({ name: 'react-storybook' }),
+          presets: ['env', 'react'],
+          plugins: [
+            'transform-flow-strip-types',
+            'transform-decorators-legacy',
+            'transform-class-properties',
+            'lodash',
+          ],
+        },
       }, {
         test: /\.scss$/,
         loaders: [
-          'style?sourceMap',
-          'css?sourceMap&modules&localIdentName=[name]_[local]&importLoaders=1',
-          'sass?sourceMap',
+          'style-loader?sourceMap',
+          'css-loader?sourceMap&modules&localIdentName=[name]_[local]&importLoaders=1',
+          'sass-loader?sourceMap',
         ]
       }
     ]
