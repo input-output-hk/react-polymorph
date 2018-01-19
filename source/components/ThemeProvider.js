59f27d363fb6ffea8f2e4aeecb012de908bc84a8
MarcusHurney <marcushurney@gmail.com>
2018-01-18 19:35:56 -0800

    Adds comments and context for passing a theme to nested child components.

--- source/components/ThemeProvider.js
+++ source/components/ThemeProvider.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react';
-import * as pickBy from 'lodash.pickby';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 
+// utility function for composing a component's theme object
+// with any themeOverrides
 import composeTheme from '../utils/composeTheme.js';
 
 // import the Root Theme API object which specifies the shape
-// of a theme for every component in this library
+// of a theme for every component in this library, used in composeLibraryTheme
 import { rootThemeAPI } from '../themes/API';
 
-export class ThemeProvider extends Component {
-  static propTypes = {
-    theme: PropTypes.obj,
-    themeOverrides: PropTypes.obj,
-    children: PropTypes.element
-  };
-
-  static defaultProps = { theme: {}, themeOverrides: {} };
-
+class ThemeProvider extends Component {
   constructor(props) {
     super(props);
     const { children, theme, themeOverrides } = props;
+
     this.state = {
-      composedTheme: this.composeFinalTheme(theme, themeOverrides, rootThemeAPI)
+      composedTheme: this.composeLibraryTheme(
+        theme,
+        themeOverrides,
+        rootThemeAPI
+      )
     };
   }
+
+  // prevents performance issues associated with frequently rerendering all
+  // children of ThemeProvider by checking to see if the passed theme and/or
+  // themeOverrides props have changed before a rerender
 
   componentWillReceiveProps(nextProps) {
     const { children, theme, themeOverrides } = nextProps;
 
-    const changedProps = pickBy(
+    const changedProps = _.pickBy(
       { theme, themeOverrides },
       (value, key) => this.props[key] !== value
     );
 
     if (Object.keys(changedProps).length > 0) {
       this.setState({
-        composedTheme: this.composeFinalTheme(
+        composedTheme: this.composeLibraryTheme(
           theme,
           themeOverrides,
           rootThemeAPI
@@ -44,7 +47,16 @@ export class ThemeProvider extends Component {
     }
   }
 
-  composeFinalTheme = (theme, themeOverrides, rootThemeAPI) => {
+  // composeLibraryTheme returns a single obj containing theme definitions
+  // for every component in the React Polymorph library
+  // every key on the returned obj is named after a component in the library
+  // and each key's value are the specific css definitions for each element
+  // in a component. The structure of a component's theme is defined in
+  // themes/API -- Each component has its own file containing an obj
+  // which demarcates its structure. This aids rapid development of themes in a
+  // reuseable + composable manner
+
+  composeLibraryTheme = (theme, themeOverrides, rootThemeAPI) => {
     if (_.isEmpty(themeOverrides)) {
       return theme;
     } else {
@@ -57,6 +69,7 @@ export class ThemeProvider extends Component {
           // delete property from composedTheme because it will remain empty
           // not every property on rootThemeAPI should be returned
           // only the non-empty props that this.props.theme contains
+
           delete composedTheme[componentName];
         }
 
@@ -73,8 +86,36 @@ export class ThemeProvider extends Component {
     }
   };
 
-  render() {
+  // Set the share value
+  getChildContext() {
     const { composedTheme } = this.state;
-    return React.cloneElement(this.props.children, { theme: composedTheme });
+
+    return {
+      theme: { ...composedTheme }
+    };
+  }
+
+  // all children of ThemeProvider HOC are passed the high level theme object
+  // composed with any custom config passed via this.props.themeOverrides
+
+  render() {
+    return <div>{this.props.children}</div>;
   }
 }
+
+ThemeProvider.childContextTypes = {
+  theme: PropTypes.object
+};
+
+ThemeProvider.defaultProps = {
+  theme: { ...rootThemeAPI },
+  themeOverrides: {}
+};
+
+ThemeProvider.propTypes = {
+  theme: PropTypes.object,
+  themeOverrides: PropTypes.object,
+  children: PropTypes.element
+};
+
+export default ThemeProvider;
