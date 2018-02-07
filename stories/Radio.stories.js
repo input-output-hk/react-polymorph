761ac807de5268c9149804268d0d2a8cd4c7373f
Cat Tien Nguyen <Cattien@Cats-MacBook-Air.local>
2018-02-07 13:55:08 -0800

    Adds Radio and theme updates from refactor branch to master

--- stories/Radio.stories.js
+++ stories/Radio.stories.js
@@ -1,67 +1,140 @@
 import React from 'react';
+
+// storybook
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { observable, action as mobxAction } from 'mobx';
-import PropsObserver from './support/PropsObserver';
+import { withState } from '@dump247/storybook-state';
+
+// components
+import ThemeProvider from '../source/components/ThemeProvider';
 import Radio from '../source/components/Radio';
+
+// skins
 import SimpleRadioSkin from '../source/skins/simple/RadioSkin';
+
+// themes
+import { SimpleRadioTheme } from '../source/themes/simple';
+
+// custom styles & themeOverrides
 import styles from './Radio.stories.scss';
+import themeOverrides from './styles/customRadio.scss';
 
 storiesOf('Radio', module)
+  .addDecorator(story => {
+    const SimpleTheme = { radio: { ...SimpleRadioTheme } };
 
-  .addDecorator((story) => {
-    const onChangeAction = action('onChange');
-    const state = observable({
-      selected: false,
-      onChange: mobxAction((value, event) => {
-        state.selected = value;
-        onChangeAction(value, event);
-      })
-    });
-    return <PropsObserver propsForChildren={state}>{story()}</PropsObserver>;
+    return <ThemeProvider theme={SimpleTheme}>{story()}</ThemeProvider>;
   })
 
   // ====== Stories ======
 
-  .add('plain', () => <Radio skin={<SimpleRadioSkin />} />)
+  .add(
+    'plain',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+        />
+      </div>
+    ))
+  )
 
-  .add('disabled', () => <Radio disabled skin={<SimpleRadioSkin />} />)
+  .add(
+    'disabled',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio disabled skin={SimpleRadioSkin} />
+      </div>
+    ))
+  )
 
-  .add('short label', () => (
-    <Radio
-      label="My radio"
-      skin={<SimpleRadioSkin />}
-    />
-  ))
+  .add(
+    'short label',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          label="My radio"
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+        />
+      </div>
+    ))
+  )
 
-  .add('custom className', () => (
-    <Radio
-      label="My radio"
-      skin={<SimpleRadioSkin />}
-      className={styles.padding}
-    />
-  ))
+  .add(
+    'custom className',
+    withState({ selected: false }, store => (
+      <Radio
+        className={styles.padding}
+        selected={store.state.selected}
+        onChange={() => store.set({ selected: !store.state.selected })}
+        skin={SimpleRadioSkin}
+      />
+    ))
+  )
 
-  .add('disabled with label', () => (
-    <Radio
-      disabled
-      label="My radio"
-      skin={<SimpleRadioSkin />}
-    />
-  ))
+  .add(
+    'disabled with label',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          disabled
+          label="My radio"
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+        />
+      </div>
+    ))
+  )
 
-  .add('long label', () => (
-    <Radio
-      skin={<SimpleRadioSkin />}
-      label="I understand that if this application is moved to another device or deleted,
-             my money can be only recovered with the backup phrase which
-             were written down in a secure place"
-    />
-  ))
+  .add(
+    'long label',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          label="I understand that if this application is moved to another device or deleted,
+                 my money can be only recovered with the backup phrase which
+                 were written down in a secure place"
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+        />
+      </div>
+    ))
+  )
 
-  .add('html label', () => (
-    <Radio
-      skin={<SimpleRadioSkin />}
-      label={<div>Example for a <strong>bold</strong> word in an html label</div>}
-    />
-  ));
+  .add(
+    'html label',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+          label={
+            <div>
+              Example for a <strong>bold</strong> word in an html label
+            </div>
+          }
+        />
+      </div>
+    ))
+  )
+
+  .add(
+    'composed theme',
+    withState({ selected: false }, store => (
+      <div className={styles.container}>
+        <Radio
+          themeOverrides={themeOverrides}
+          label="Radio with a composed theme"
+          selected={store.state.selected}
+          onChange={() => store.set({ selected: !store.state.selected })}
+          skin={SimpleRadioSkin}
+        />
+      </div>
+    ))
+  );
