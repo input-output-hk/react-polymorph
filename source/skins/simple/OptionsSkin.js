0c15b97d006b20d3cbd3bf8cc8f044a9e1bbfbd9
MarcusHurney <marcushurney@gmail.com>
2018-04-12 00:58:51 -0700

    Implements renderOptions.

--- source/skins/simple/OptionsSkin.js
+++ source/skins/simple/OptionsSkin.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
 // external libraries
-import classnames from 'classnames';
+import classnames from "classnames";
 
 // components
-import { Bubble } from '../../components';
+import { Bubble } from "../../components";
 
 // skins
-import { BubbleSkin } from './';
+import { BubbleSkin } from "./";
 
 export default props => {
   const {
     getOptionProps,
-    children,
     render,
     theme,
     themeId,
@@ -34,28 +33,20 @@ export default props => {
   const sortedOptions = isOpeningUpward ? options.slice().reverse() : options;
 
   const renderOptions = () => {
-    // if there are results + the user wants to control rendering
-    // via the render or children function
-    if ((!noResults && children) || (!noResults && render)) {
-      // delegate the use of sortedOptions and getOptionProps to the user
-      // by calling children/render func with those two params
-      // TODO: check to call either children OR render
-      return children({ sortedOptions, getOptionProps });
-      // children
-      //   ? children({ sortedOptions, getOptionProps })
-      //   : render({ sortedOptions, getOptionProps });
-    }
-
-    // there are results and user did not pass render or children func
-    if (!noResults) {
+    // check for user's custom render function
+    // if Options is being rendered via Autocomplete,
+    // the value of props.render is renderOptions passed down from AutocompleteSkin
+    if (!noResults && render) {
+      // call custom render function
+      return render(getOptionProps);
+    } else if (!noResults && !render) {
+      // render default skin
       return sortedOptions.map((option, index) => (
         <li
           key={index}
           className={classnames([
             theme[themeId].option,
-            isHighlightedOption(index)
-              ? theme[themeId].highlightedOption
-              : null,
+            isHighlightedOption(index) ? theme[themeId].highlightedOption : null,
             option.isDisabled ? theme[themeId].disabledOption : null
           ])}
           onClick={event => handleClickOnOption(option, event)}
@@ -63,11 +54,11 @@ export default props => {
         >
           {optionRenderer
             ? optionRenderer(option)
-            : typeof option === 'object' ? option.label : option}
+            : typeof option === "object" ? option.label : option}
         </li>
       ));
     } else {
-      // there are no results
+      // render no results message
       return <li className={theme[themeId].option}>{noResultsMessage}</li>;
     }
   };
