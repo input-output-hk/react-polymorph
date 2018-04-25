fb90853ff50677d7ff98f32224031da5398a9f83
MarcusHurney <marcushurney@gmail.com>
2018-04-24 22:31:00 -0700

    [DDW-112] Adds flow type declarations to all utility functions.

--- source/utils/props.js
+++ source/utils/props.js
@@ -1,18 +1,14 @@
-import { oneOfType, string, element } from 'prop-types';
+// @flow
 import filterReactDomProps from 'filter-react-dom-props';
 
 // filters out / prevents invalid props from being rendered to the dom
 // which would generate an error/warning
 const pickDOMProps = filterReactDomProps;
 
-// ensures that a prop passed to a component is either a string or element
-const StringOrElement = oneOfType([string, element]);
-
-const composeFunctions = (...fns) => (...args) =>
+const composeFunctions = (...fns: [Function]) => (...args: [any]) =>
   fns.forEach(fn => fn && fn(...args));
 
 export default {
   pickDOMProps,
-  StringOrElement,
   composeFunctions
 };
