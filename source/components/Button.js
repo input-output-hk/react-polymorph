42706abde5c29b3cd9606eb9be5fd1c8c2c115e5
MarcusHurney <marcushurney@gmail.com>
2018-04-24 21:55:52 -0700

    [DDW-112] Adds flow and eslint declarations for Button.js. Fixes eslint errors.

--- source/components/Button.js
+++ source/components/Button.js
@@ -1,28 +1,33 @@
+// @flow
 import React, { Component } from 'react';
-import { string, bool, func, object, shape } from 'prop-types';
+import type { ComponentType } from 'react';
 import { withTheme } from '../themes/withTheme';
 
 // internal utility functions
-import { StringOrElement, composeTheme, addThemeId } from '../utils';
+import { composeTheme, addThemeId } from '../utils';
 
 // import constants
 import { IDENTIFIERS } from '../themes/API';
 
-class Button extends Component {
-  static propTypes = {
-    context: shape({
-      theme: object,
-      ROOT_THEME_API: object
-    }),
-    disabled: bool,
-    label: StringOrElement,
-    onClick: func,
-    skin: func.isRequired,
-    theme: object,
-    themeId: string,
-    themeOverrides: object // custom css/scss from user that adheres to component's theme API
-  };
+type Props = {
+  context: {
+    theme: Object,
+    ROOT_THEME_API: Object
+  },
+  disabled: boolean,
+  label: string | Element,
+  onClick: Function,
+  skin: ComponentType<any>,
+  theme: Object, // will take precedence over theme in context if passed
+  themeId: string,
+  themeOverrides: Object // custom css/scss from user that adheres to component's theme API
+};
 
+type State = {
+  composedTheme: Object
+};
+
+class Button extends Component<Props, State> {
   static defaultProps = {
     disabled: false,
     theme: null,
@@ -30,7 +35,7 @@ class Button extends Component {
     themeOverrides: {}
   };
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
 
     const { context, themeId, theme, themeOverrides } = props;
