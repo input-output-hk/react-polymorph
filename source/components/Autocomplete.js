8a48c57670446db66230484c18556a4074b0f37c
MarcusHurney <marcushurney@gmail.com>
2018-04-24 21:48:28 -0700

    [DDW-112] Adds flow and eslint declarations for Autocomplete. Fixes eslint errors.

--- source/components/Autocomplete.js
+++ source/components/Autocomplete.js
@@ -1,55 +1,54 @@
+// @flow
 import React, { Component } from 'react';
-import {
-  bool,
-  func,
-  object,
-  number,
-  array,
-  string,
-  instanceOf,
-  shape
-} from 'prop-types';
-import { withTheme } from '../themes/withTheme';
+import type { ComponentType } from 'react';
 
 // external libraries
 import _ from 'lodash';
 
 // internal utility functions
-import {
-  StringOrElement,
-  composeTheme,
-  composeFunctions,
-  addThemeId
-} from '../utils';
+import { withTheme } from '../themes/withTheme';
+import { composeTheme, composeFunctions, addThemeId } from '../utils';
 
 import { IDENTIFIERS } from '../themes/API';
 
-class Autocomplete extends Component {
-  static propTypes = {
-    context: shape({
-      theme: object,
-      ROOT_THEME_API: object
-    }),
-    error: StringOrElement,
-    invalidCharsRegex: instanceOf(RegExp),
-    isOpeningUpward: bool,
-    label: StringOrElement,
-    maxSelections: number,
-    maxVisibleOptions: number,
-    multipleSameSelections: bool,
-    onChange: func,
-    options: array,
-    selectedOptions: array,
-    placeholder: string,
-    render: func,
-    renderSelections: func,
-    renderOptions: func,
-    skin: func.isRequired,
-    sortAlphabetically: bool,
-    theme: object,
-    themeId: string,
-    themeOverrides: object
-  };
+type Props = {
+  context: {
+    theme: Object,
+    ROOT_THEME_API: Object
+  },
+  error: string,
+  invalidCharsRegex: RegExp,
+  isOpeningUpward: boolean,
+  label: string | Element,
+  maxSelections: number,
+  maxVisibleOptions: number,
+  multipleSameSelections: boolean,
+  onChange: Function,
+  options: Array<any>,
+  selectedOptions: Array<any>,
+  placeholder: string,
+  renderSelections: Function,
+  renderOptions: Function,
+  skin: ComponentType<any>,
+  sortAlphabetically: boolean,
+  theme: Object, // will take precedence over theme in context if passed
+  themeId: string,
+  themeOverrides: Object
+};
+
+type State = {
+  inputValue: string,
+  error: string,
+  selectedOptions: Array<any>,
+  filteredOptions: Array<any>,
+  isOpen: boolean,
+  composedTheme: Object
+};
+
+class Autocomplete extends Component<Props, State> {
+  rootElement: ?Element;
+  inputElement: HTMLInputElement;
+  suggestionsElement: ?Element;
 
   static defaultProps = {
     error: null,
@@ -64,7 +63,7 @@ class Autocomplete extends Component {
     themeOverrides: {}
   };
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
 
     const {
@@ -114,7 +113,7 @@ class Autocomplete extends Component {
   };
 
   // checks for backspace in order to delete the last selected option
-  onKeyDown = event => {
+  onKeyDown = (event: SyntheticKeyboardEvent<>) => {
     if (
       event.keyCode === 8 &&
       !event.target.value &&
@@ -126,11 +125,11 @@ class Autocomplete extends Component {
   };
 
   // onChange handler for input element in AutocompleteSkin
-  handleInputChange = event => {
+  handleInputChange = (event: SyntheticInputEvent<HTMLInputElement>) => {
     const value = event.target.value;
 
     // filter out invalid characters
-    const filteredValue = this._filterInvalidChars(event.target.value);
+    const filteredValue = this._filterInvalidChars(value);
 
     // filter options
     const filteredOptions = this._filterOptions(filteredValue);
@@ -144,11 +143,14 @@ class Autocomplete extends Component {
   };
 
   // passed to Options onChange handler in AutocompleteSkin
-  handleChange = (option, event) => {
+  handleChange = (option: any, event: SyntheticEvent<>) => {
     this.updateSelectedOptions(event, option);
   };
 
-  updateSelectedOptions = (event, selectedOption = null) => {
+  updateSelectedOptions = (
+    event: SyntheticEvent<>,
+    selectedOption: any = null
+  ) => {
     const canMoreOptionsBeSelected =
       this.state.selectedOptions.length < this.props.maxSelections;
     const areFilteredOptionsAvailable =
@@ -159,7 +161,7 @@ class Autocomplete extends Component {
       (canMoreOptionsBeSelected && areFilteredOptionsAvailable)
     ) {
       if (!selectedOption) return;
-      let option = selectedOption.trim();
+      const option = selectedOption.trim();
       const optionCanBeSelected =
         (this.state.selectedOptions.indexOf(option) < 0 &&
           !this.props.multipleSameSelections) ||
@@ -175,21 +177,26 @@ class Autocomplete extends Component {
     this.setState({ inputValue: '' });
   };
 
-  removeOption = (index, event) => {
+  removeOption = (index: number, event: SyntheticEvent<>) => {
     const selectedOptions = this.state.selectedOptions;
     _.pullAt(selectedOptions, index);
     this.selectionChanged(selectedOptions, event);
     this.setState({ selectedOptions });
   };
 
-  selectionChanged = (selectedOptions, event) => {
+  selectionChanged = (
+    selectedOptions: Array<any>,
+    event: SyntheticEvent<any>
+  ) => {
     if (this.props.onChange) this.props.onChange(selectedOptions, event);
   };
 
   // returns an object containing props, theme, and method handlers
   // associated with rendering this.state.selectedOptions, the user can call
   // this in the body of the renderSelections function
-  getSelectionProps = ({ removeSelection }) => {
+  getSelectionProps = ({
+    removeSelection
+  }: { removeSelection: Function } = {}) => {
     const { themeId } = this.props;
     const { inputValue, isOpen, selectedOptions, composedTheme } = this.state;
     return {
@@ -197,7 +204,7 @@ class Autocomplete extends Component {
       isOpen,
       selectedOptions,
       theme: composedTheme[themeId],
-      removeSelection: (index, event) =>
+      removeSelection: (index: number, event: SyntheticEvent<>) =>
         // the user's custom removeSelection event handler is composed with
         // the internal functionality of Autocomplete (this.removeOption)
         composeFunctions(removeSelection, this.removeOption)(index, event)
@@ -241,15 +248,16 @@ class Autocomplete extends Component {
   // ======== PRIVATE METHOD ==========
 
   _removeOptions = () => {
-    this.selectionChanged([]);
+    const { onChange } = this.props;
+    onChange ? onChange([]) : null;
     this.setState({ selectedOptions: [], inputValue: '' });
   };
 
-  _filterOptions = value => {
+  _filterOptions = (value: string) => {
     let filteredOptions = [];
 
     if (value !== '') {
-      _.some(this.props.options, function(option) {
+      _.some(this.props.options, (option) => {
         if (_.startsWith(option, value)) {
           filteredOptions.push(option);
         }
@@ -261,7 +269,7 @@ class Autocomplete extends Component {
     return filteredOptions;
   };
 
-  _filterInvalidChars = value => {
+  _filterInvalidChars = (value: string) => {
     let filteredValue = '';
 
     if (this.props.invalidCharsRegex.test(value)) {
