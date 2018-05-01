4abd90e6b83a8b80d2ec732a2cf4ff1b94ede06d
MarcusHurney <marcushurney@gmail.com>
2018-05-01 00:13:09 -0700

    [DDW-201] Adds jest configuration files and jest rules to package.json. Adds __test__ directory with basic snapshot test for an anchor tag

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "moduleFileExtensions": [
+    "js",
+    "jsx"
+  ],
+  "moduleNameMapper": {
+    "Autocomplete": "<rootDir>/source/components/Autocomplete.js",
+    "Bubble": "<rootDir>/source/components/Bubble.js",
+    "Button": "<rootDir>/source/components/Button.js",
+    "Checkbox": "<rootDir>/source/components/Checkbox.js",
+    "FormField": "<rootDir>/source/components/FormField.js",
+    "Input": "<rootDir>/source/components/Input.js",
+    "Modal": "<rootDir>/source/components/Modal.js",
+    "NumericInput": "<rootDir>/source/components/NumericInput.js",
+    "Options": "<rootDir>/source/components/Options.js",
+    "Radio": "<rootDir>/source/components/Radio.js",
+    "Select": "<rootDir>/source/components/Select.js",
+    "TextArea": "<rootDir>/source/components/TextArea.js",
+    "Tooltip": "<rootDir>/source/components/Tooltip.js"
+  }
+}
