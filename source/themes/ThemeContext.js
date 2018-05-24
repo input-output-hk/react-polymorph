17a8ccefab982bfee98badeed07d6ddee9377c43
MarcusHurney <marcushurney@gmail.com>
2018-05-24 19:00:42 +0700

    [DDW-281] Implements createContext polyfill

--- source/themes/ThemeContext.js
+++ source/themes/ThemeContext.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import createReactContext, { type Context } from 'create-react-context';
 import SimpleTheme from './simple';
 import ROOT_THEME_API from './API';
 
@@ -8,8 +9,23 @@ import ROOT_THEME_API from './API';
 // same goes for "this.props.context.ROOT_THEME_API"
 // if the user passes ThemeProvider a theme and/or ROOT_THEME_API,
 // these default values are overwritten
-// $FlowFixMe
-export const ThemeContext = React.createContext({
+
+type Theme = {
+  theme: Object,
+  ROOT_THEME_API: Object
+};
+
+// check for createContext method on React
+let createContext;
+if (React.createContext) {
+  // use React.createContext
+  createContext = React.createContext;
+} else {
+  // use create-react-context polyfill
+  createContext = createReactContext;
+}
+
+export const ThemeContext: Context<Theme> = createContext({
   theme: SimpleTheme,
   ROOT_THEME_API
 });
--- source/themes/withTheme.js
+++ source/themes/withTheme.js
@@ -1,14 +1,14 @@
 // @flow
 import React from 'react';
 import type { ComponentType, Ref } from 'react';
+import forwardRef from 'create-react-ref/lib/forwardRef';
 import { ThemeContext } from './ThemeContext';
 
 // withTheme is a HOC that takes a Component as a parameter
 // and returns that Component wrapped within ThemeContext.Consumer.
 // Any additional props and refs are forwarded to the returned Component.
 export const withTheme = (Component: ComponentType<any>) =>
-  // $FlowFixMe
-  React.forwardRef((props: {}, ref: Ref<any>) => (
+  forwardRef((props: {}, ref: Ref<any>) => (
     <ThemeContext.Consumer>
       {context => <Component context={context} ref={ref} {...props} />}
     </ThemeContext.Consumer>
