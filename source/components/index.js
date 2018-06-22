9e829af69b611f8f7a419262121598bc646555ef
MarcusHurney <marcushurney@gmail.com>
2018-06-21 23:49:03 -0700

    [DDW-307] Exports all components as named exports instead of importing the default from each component file and re-exporting as a named export.

--- source/components/index.js
+++ source/components/index.js
@@ -1,16 +1,16 @@
 // @flow
 // exports all components as named exports
-export { default as ThemeProvider } from './ThemeProvider';
-export { default as Autocomplete } from './Autocomplete';
-export { default as Bubble } from './Bubble';
-export { default as Button } from './Button';
-export { default as Checkbox } from './Checkbox';
-export { default as FormField } from './FormField';
-export { default as Input } from './Input';
-export { default as NumericInput } from './NumericInput';
-export { default as Modal } from './Modal';
-export { default as Options } from './Options';
-export { default as Radio } from './Radio';
-export { default as Select } from './Select';
-export { default as TextArea } from './TextArea';
-export { default as Tooltip } from './Tooltip';
+export { ThemeProvider } from './ThemeProvider';
+export { Autocomplete } from './Autocomplete';
+export { Bubble } from './Bubble';
+export { Button } from './Button';
+export { Checkbox } from './Checkbox';
+export { FormField } from './FormField';
+export { Input } from './Input';
+export { NumericInput } from './NumericInput';
+export { Modal } from './Modal';
+export { Options } from './Options';
+export { Radio } from './Radio';
+export { Select } from './Select';
+export { TextArea } from './TextArea';
+export { Tooltip } from './Tooltip';
