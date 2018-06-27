0be930e475bbe24195c1dff44f1d29fcdad04fb3
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-27 13:56:13 +0200

    [DDW-336] fix state handling for input focus / blur example

--- stories/Input.stories.js
+++ stories/Input.stories.js
@@ -130,8 +130,8 @@ storiesOf('Input', module)
         value={store.state.value}
         placeholder="onFocus / onBlur"
         onChange={value => store.set({ value })}
-        onFocus={() => store.set({ focused: true })}
-        onBlur={() => store.set({ blurred: true })}
+        onFocus={() => store.set({ focused: true, blurred: false })}
+        onBlur={() => store.set({ blurred: true, focused: false })}
         skin={InputSkin}
       />
     ))
