61113008d579c5eef1d7aeec7800de4a58088add
MarcusHurney <marcushurney@gmail.com>
2018-07-10 23:10:19 -0700

    [DDW-27] Removes react-inline-svg and svg imports from LoadingSpinnerSkin

--- source/skins/simple/LoadingSpinnerSkin.js
+++ source/skins/simple/LoadingSpinnerSkin.js
@@ -1,32 +1,21 @@
 // @flow
 import React from 'react';
-import type { Ref } from 'react';
 
 // external libraries
-import SVGInline from 'react-svg-inline';
 import classnames from 'classnames';
-
-// internal assets
-import spinnerIconBig from '../../themes/simple/assets/spinner-big.inline.svg';
-import spinnerIconSmall from '../../themes/simple/assets/spinner-small.inline.svg';
 
 type Props = {
   big: boolean,
   className: string,
-  rootRef: Ref<*>,
   theme: Object,
-  themeId: string
+  themeId: string,
+  visible: boolean
 };
 
 export const LoadingSpinnerSkin = (props: Props) => {
-  const { big, rootRef, className, themeId } = props;
+  const { big, className, themeId, visible } = props;
   const theme = props.theme[themeId];
-  const icon = big ? spinnerIconBig : spinnerIconSmall;
-  const sizeStyle = big ? theme.big : theme.small;
+  const size = big ? theme.big : theme.small;
 
-  return (
-    <div ref={rootRef} className={classnames([className, theme.root, sizeStyle])}>
-      <SVGInline svg={icon} className={theme.icon} />
-    </div>
-  );
+  return visible ? <div className={classnames([className, theme.root, size])} /> : null;
 };
