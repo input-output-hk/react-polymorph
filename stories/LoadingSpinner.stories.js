5fedeabed29a2d69762d2b5bca12235bc2df9583
MarcusHurney <marcushurney@gmail.com>
2018-07-10 23:13:13 -0700

    [DDW-27] Adds 5 new LoadingSpinner stories

--- stories/LoadingSpinner.stories.js
+++ stories/LoadingSpinner.stories.js
@@ -10,24 +10,53 @@ import { LoadingSpinner } from '../source/components/LoadingSpinner';
 // skins
 import { LoadingSpinnerSkin } from '../source/skins/simple/LoadingSpinnerSkin';
 
-// // themes
-// import CustomInputTheme from './theme-customizations/Input.custom.scss';
-//
-// // theme overrides and identifiers
-// import themeOverrides from './theme-overrides/customInput.scss';
+// // theme overrides and styles
+import dashedSpinner from './theme-overrides/dashedSpinner.scss';
+import adaSpinner from './theme-overrides/adaSpinner.scss';
+import daedalusSpinner from './theme-overrides/daedalusSpinner.scss';
+import styles from './LoadingSpinner.stories.scss';
 
 storiesOf('LoadingSpinner', module)
   // ====== LoadingSpinner Stories ======
 
-  .add('big', () => (
-    <LoadingSpinner
-      big
-      skin={LoadingSpinnerSkin}
-    />
+  .add('small', () => (
+    <div className={styles.marginWrapper}>
+      <LoadingSpinner skin={LoadingSpinnerSkin} />
+    </div>
   ))
 
-  .add('small', () => (
-    <LoadingSpinner
-      skin={LoadingSpinnerSkin}
-    />
+  .add('big', () => (
+    <div className={styles.marginWrapper}>
+      <LoadingSpinner big skin={LoadingSpinnerSkin} />
+    </div>
+  ))
+
+  .add('theme overrides - Dashed spinner', () => (
+    <div className={styles.marginWrapper}>
+      <LoadingSpinner
+        big
+        themeOverrides={dashedSpinner}
+        skin={LoadingSpinnerSkin}
+      />
+    </div>
+  ))
+
+  .add('theme overrides - ADA spinner', () => (
+    <div className={styles.marginWrapper}>
+      <LoadingSpinner
+        big
+        themeOverrides={adaSpinner}
+        skin={LoadingSpinnerSkin}
+      />
+    </div>
+  ))
+
+  .add('theme overrides - Daedalus spinner', () => (
+    <div className={styles.marginWrapper}>
+      <LoadingSpinner
+        big
+        themeOverrides={daedalusSpinner}
+        skin={LoadingSpinnerSkin}
+      />
+    </div>
   ));
