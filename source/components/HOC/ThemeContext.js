7d5a00532f451c080ffe3339deed7434c015d93f
MarcusHurney <marcushurney@gmail.com>
2018-07-12 19:43:24 -0700

    [DDW-25] Removes theme imports for every component within ThemeContext.js

--- source/components/HOC/ThemeContext.js
+++ source/components/HOC/ThemeContext.js
@@ -3,24 +3,8 @@ import React from 'react';
 import createReactContext, { type Context } from 'create-react-context';
 
 // import constants
-import { IDENTIFIERS, ROOT_THEME_API } from '../../themes/API';
-
-// import simple theme file for every component
-import SimpleAutocomplete from '../../themes/simple/SimpleAutocomplete.scss';
-import SimpleBubble from '../../themes/simple/SimpleBubble.scss';
-import SimpleButton from '../../themes/simple/SimpleButton.scss';
-import SimpleCheckbox from '../../themes/simple/SimpleCheckbox.scss';
-import SimpleFormField from '../../themes/simple/SimpleFormField.scss';
-import SimpleInput from '../../themes/simple/SimpleInput.scss';
-import SimpleModal from '../../themes/simple/SimpleModal.scss';
-import SimpleOptions from '../../themes/simple/SimpleOptions.scss';
-import SimpleProgressBar from '../../themes/simple/SimpleProgressBar.scss';
-import SimpleRadio from '../../themes/simple/SimpleRadio.scss';
-import SimpleSelect from '../../themes/simple/SimpleSelect.scss';
-import SimpleSwitch from '../../themes/simple/SimpleSwitch.scss';
-import SimpleTextArea from '../../themes/simple/SimpleTextArea.scss';
-import SimpleToggler from '../../themes/simple/SimpleToggler.scss';
-import SimpleTooltip from '../../themes/simple/SimpleTooltip.scss';
+import { ROOT_THEME_API } from '../../themes/API';
+import { SimpleTheme } from '../../themes/simple/';
 
 // components that are NOT directly nested within a ThemeProvider
 // can access simple theme as "this.props.context.theme",
@@ -41,25 +25,6 @@ if (React.createContext) {
 type Theme = {
   theme: Object,
   ROOT_THEME_API: Object
-};
-
-// construct SimpleTheme object
-const SimpleTheme = {
-  [IDENTIFIERS.AUTOCOMPLETE]: SimpleAutocomplete,
-  [IDENTIFIERS.BUBBLE]: SimpleBubble,
-  [IDENTIFIERS.BUTTON]: SimpleButton,
-  [IDENTIFIERS.CHECKBOX]: SimpleCheckbox,
-  [IDENTIFIERS.FORM_FIELD]: SimpleFormField,
-  [IDENTIFIERS.INPUT]: SimpleInput,
-  [IDENTIFIERS.MODAL]: SimpleModal,
-  [IDENTIFIERS.OPTIONS]: SimpleOptions,
-  [IDENTIFIERS.PROGRESS_BAR]: SimpleProgressBar,
-  [IDENTIFIERS.RADIO]: SimpleRadio,
-  [IDENTIFIERS.SELECT]: SimpleSelect,
-  [IDENTIFIERS.SWITCH]: SimpleSwitch,
-  [IDENTIFIERS.TEXT_AREA]: SimpleTextArea,
-  [IDENTIFIERS.TOGGLER]: SimpleToggler,
-  [IDENTIFIERS.TOOLTIP]: SimpleTooltip
 };
 
 const defaultContext = { theme: SimpleTheme, ROOT_THEME_API };
