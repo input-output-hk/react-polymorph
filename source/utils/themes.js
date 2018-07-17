45b7068a30a86e7beee8ce7c90f109e03dab6a28
MarcusHurney <marcushurney@gmail.com>
2018-07-16 22:23:41 -0700

    [DDW-197] Adds formatFlexProps utility function to utils/themes.js

--- source/utils/themes.js
+++ source/utils/themes.js
@@ -1,8 +1,86 @@
 // @flow
-import { isEmpty, cloneDeep } from 'lodash';
+import { isEmpty, cloneDeep, pickBy } from 'lodash';
 
 export const appendToProperty = (dest: {}, name: string, value: string) => {
   dest[name] === '' ? (dest[name] = value) : (dest[name] += ' ' + value);
+};
+
+export const formatFlexProps = (activeProps: Object) => {
+  if (isEmpty(activeProps)) { return; }
+
+  const { justifyContent, alignItems, center } = activeProps;
+
+  // the "center" prop sets both "justify-content" and 'align-items' to center
+  if (center === true) {
+    // setting the values individually is uneccessary
+    // only the "center" prop will be forwarded
+    activeProps.justifyContent = false;
+    activeProps.alignItems = false;
+    // finished, return early
+    return pickBy(activeProps);
+  }
+
+  if (justifyContent) {
+    switch (justifyContent) {
+      case 'flex-start':
+        activeProps.justifyStart = true;
+        break;
+      case 'flex-end':
+        activeProps.justifyEnd = true;
+        break;
+      case 'center':
+        activeProps.justifyCenter = true;
+        break;
+      case 'space-between':
+        activeProps.justifyBetween = true;
+        break;
+      case 'space-around':
+        activeProps.justifyAround = true;
+        break;
+      default:
+        break;
+    }
+    activeProps.justifyContent = false;
+  }
+
+  if (alignItems) {
+    switch (alignItems) {
+      case 'flex-start':
+        activeProps.alignStart = true;
+        break;
+      case 'flex-end':
+        activeProps.alignEnd = true;
+        break;
+      case 'center':
+        activeProps.alignCenter = true;
+        break;
+      case 'baseline':
+        activeProps.alignBaseline = true;
+        break;
+      default:
+        break;
+    }
+    activeProps.alignItems = false;
+  }
+
+  return pickBy(activeProps);
+};
+
+export const composeBaseStyles = (
+  baseStyles: Object,
+  stylesToAdd: Object,
+  activeClasses: Array) => {
+  if (!activeClasses.length) { return baseStyles; }
+
+  const composedBase = { ...baseStyles };
+
+  activeClasses.forEach(className => {
+    if (Object.hasOwnProperty.call(stylesToAdd, className) && stylesToAdd[className]) {
+      composedBase.base += ` ${stylesToAdd[className]}`;
+    }
+  });
+
+  return composedBase;
 };
 
 export const composeComponentStyles = (componentStyles: {}, componentTheme: {}) => {
