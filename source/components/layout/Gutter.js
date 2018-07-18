5fdeea5159a56c0284ca4951122a70fadcb6fe44
MarcusHurney <marcushurney@gmail.com>
2018-07-17 23:49:12 -0700

    [DDW-197] Adds Gutter component

---
+++ source/components/layout/Gutter.js
@@ -0,0 +1,32 @@
+// @flow
+import React from 'react';
+
+// components
+import { Base } from './Base';
+
+// styles
+import gutterStyles from '../../themes/simple/layout/Gutter.scss';
+
+// utilities
+import { numberToPx } from '../../utils/props';
+
+type Props = {
+  className: string,
+  padding: string | number
+};
+
+export const Gutter = (props: Props) => {
+  const { children, className } = props;
+  const padding = props.padding ? numberToPx(props.padding) : null;
+
+  return (
+    <Base
+      activeClasses={['gutter']}
+      className={className}
+      inlineStyles={{ padding }}
+      stylesToAdd={gutterStyles}
+    >
+      {children}
+    </Base>
+  );
+};
