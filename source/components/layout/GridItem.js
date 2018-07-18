3feb7124dc78182fc894e289e954240cd660fbf9
MarcusHurney <marcushurney@gmail.com>
2018-07-17 23:48:40 -0700

    [DDW-197] Adds GridItem component

---
+++ source/components/layout/GridItem.js
@@ -0,0 +1,36 @@
+// @flow
+import React from 'react';
+import { pickBy } from 'lodash';
+
+// components
+import { Base } from './Base';
+
+// styles
+import itemStyles from '../../themes/simple/layout/GridItem.scss';
+
+// utilities
+import { formatFlexItemProps } from '../../utils/layout';
+
+type Props = {
+  alignSelf: string,
+  className: string,
+  flex: number,
+  order: number
+};
+
+export const GridItem = (props: Props) => {
+  const { children, className, order, ...flexItemProps } = props;
+
+  const activeProps = pickBy(({ flexItem: true, ...flexItemProps }));
+  const activeClasses = Object.keys(formatFlexItemProps(activeProps));
+  return (
+    <Base
+      activeClasses={activeClasses}
+      className={className}
+      inlineStyles={{ order }}
+      stylesToAdd={itemStyles}
+    >
+      {children}
+    </Base>
+  );
+};
