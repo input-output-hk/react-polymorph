80fd328c8d3cc8bfe0feb71bf61ed73e45c4df1a
MarcusHurney <marcushurney@gmail.com>
2018-07-17 23:51:46 -0700

    [DDW-197] Adds Grid and Gutter stories to Layout stories

--- stories/Layout.stories.js
+++ stories/Layout.stories.js
@@ -6,11 +6,13 @@ import { storiesOf } from '@storybook/react';
 
 // components
 import { Flex } from '../source/components/layout/Flex';
+import { Grid } from '../source/components/layout/Grid';
+import { Gutter } from '../source/components/layout/Gutter';
 
 // styles
 import styles from './Layout.stories.scss';
 
-const { wrapper } = styles;
+const { wrapper, header, main, aside, footer, box } = styles;
 
 storiesOf('Layout', module)
   // ====== Layout Stories ======
@@ -84,4 +86,41 @@ storiesOf('Layout', module)
       <Flex.Item order={5}>FlexItem 4</Flex.Item>
       <Flex.Item order={4}>FlexItem 5</Flex.Item>
     </Flex>
+  ))
+
+  .add('Grid - simple', () => (
+    <Gutter>
+      <Grid columns="75% 25%" rows="10vw 30vw 10vw" gap="1em">
+        <div className={header}>header</div>
+        <div className={main}>main</div>
+        <div className={aside}>aside</div>
+        <div className={footer}>footer</div>
+      </Grid>
+    </Gutter>
+  ))
+
+  .add('Grid - even', () => (
+    <Gutter>
+      <Grid
+        columns="repeat(auto-fill, minmax(200px, 1fr))"
+        autoRows="minmax(150px, auto)"
+        gap="1em"
+      >
+        <div className={box}>1</div>
+        <div className={box}>2</div>
+        <div className={box}>3</div>
+        <div className={box}>4</div>
+        <div className={box}>5</div>
+        <div className={box}>6</div>
+        <div className={box}>7</div>
+        <div className={box}>8</div>
+        <div className={box}>9</div>
+        <div className={box}>10</div>
+        <div className={box}>11</div>
+        <div className={box}>12</div>
+        <div className={box}>13</div>
+        <div className={box}>14</div>
+        <div className={box}>15</div>
+      </Grid>
+    </Gutter>
   ));
