c3dc8a8f41af3a010349bf70cec7397f78e17e77
MarcusHurney <marcushurney@gmail.com>
2018-07-19 04:00:16 -0700

    [DDW197] Adds formatTemplateAreas to utils/layout.js

--- source/utils/layout.js
+++ source/utils/layout.js
@@ -1,94 +1,27 @@
 // @flow
-import { isEmpty, pickBy } from 'lodash';
+export const formatTemplateAreas = (areas: Array<''>) => {
+  if (!areas || !areas.length) { return; }
 
-export const formatFlexProps = (activeProps: Object) => {
-  if (isEmpty(activeProps)) { return; }
-
-  const { justifyContent, alignItems, center } = activeProps;
-
-  // the "center" prop sets both "justify-content" and 'align-items' to center
-  if (center === true) {
-    // setting the values individually is uneccessary
-    // only the "center" prop will be forwarded
-    activeProps.justifyContent = false;
-    activeProps.alignItems = false;
-    // finished, return early
-    return pickBy(activeProps);
-  }
-
-  if (justifyContent) {
-    switch (justifyContent) {
-      case 'flex-start':
-        activeProps.justifyStart = true;
-        break;
-      case 'flex-end':
-        activeProps.justifyEnd = true;
-        break;
-      case 'center':
-        activeProps.justifyCenter = true;
-        break;
-      case 'space-between':
-        activeProps.justifyBetween = true;
-        break;
-      case 'space-around':
-        activeProps.justifyAround = true;
-        break;
-      default:
-        break;
-    }
-    activeProps.justifyContent = false;
-  }
-
-  if (alignItems) {
-    switch (alignItems) {
-      case 'flex-start':
-        activeProps.alignStart = true;
-        break;
-      case 'flex-end':
-        activeProps.alignEnd = true;
-        break;
-      case 'center':
-        activeProps.alignCenter = true;
-        break;
-      case 'baseline':
-        activeProps.alignBaseline = true;
-        break;
-      default:
-        break;
-    }
-    activeProps.alignItems = false;
-  }
-
-  return pickBy(activeProps);
+  return areas.reduce((template, row, index) => {
+    if (!index) { return `'${row}'`; }
+    return `${template} '${row}'`;
+  }, '');
 };
 
-export const formatFlexItemProps = (activeProps: Object) => {
-  if (isEmpty(activeProps)) { return; }
+export const composeBaseStyles = (
+  baseStyles: Object,
+  stylesToAdd: Object,
+  activeClasses: Array
+) => {
+  if (!activeClasses || !activeClasses.length) { return baseStyles; }
 
-  const { alignSelf } = activeProps;
+  const composedBase = { ...baseStyles };
 
-  if (alignSelf) {
-    switch (alignSelf) {
-      case 'flex-start':
-        activeProps.alignStart = true;
-        break;
-      case 'flex-end':
-        activeProps.alignEnd = true;
-        break;
-      case 'center':
-        activeProps.alignCenter = true;
-        break;
-      case 'baseline':
-        activeProps.alignBaseline = true;
-        break;
-      case 'stretch':
-        activeProps.alignStretch = true;
-        break;
-      default:
-        break;
+  activeClasses.forEach(className => {
+    if (Object.hasOwnProperty.call(stylesToAdd, className) && stylesToAdd[className]) {
+      composedBase.base += ` ${stylesToAdd[className]}`;
     }
-    activeProps.alignSelf = false;
-  }
+  });
 
-  return pickBy(activeProps);
+  return composedBase;
 };
