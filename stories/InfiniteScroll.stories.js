96241c87cf42a1fc09f5111734effd20f9dde12f
MarcusHurney <marcushurney@gmail.com>
2018-07-24 01:32:24 -0700

    [DDW-197] Improves InfiniteScroll story

--- stories/InfiniteScroll.stories.js
+++ stories/InfiniteScroll.stories.js
@@ -7,6 +7,8 @@ import { storiesOf } from '@storybook/react';
 // components
 import { InfiniteScroll } from '../source/components/InfiniteScroll';
 import { Flex } from '../source/components/layout/Flex';
+import { Header } from '../source/components/Header';
+import { HeaderSkin } from '../source/skins/simple/HeaderSkin';
 import { Grid } from '../source/components/layout/Grid';
 import { Gutter } from '../source/components/layout/Gutter';
 
@@ -17,26 +19,57 @@ const { full, scrollItem } = styles;
 
 storiesOf('InfiniteScroll', module)
   // ====== InfiniteScroll Stories ======
-
   .add('simple', () => (
-    <div className={full}>
+    <Flex center className={full}>
       <InfiniteScroll
         fetchData={setState => {
-          fetch('https://randomuser.me/api/?results=10')
+          fetch('https://randomuser.me/api/?results=12')
           .then(res => res.json())
-          .then(({ results: data }) => setState({ data }))
-          .catch(error => console.log(error))
+          .then(({ results: nextUsers }) => setState({ isLoading: true }, () =>
+            setState(prevState => ({
+              error: false,
+              isLoading: false,
+              data: [...prevState.data, ...nextUsers]
+            }))
+          ))
+          .catch(error => setState({ error }));
         }}
         renderItems={({
           data,
           error,
-          isLoading,
-          hasMoreData
-        }) => data.map((user, index) => (
-          <Flex.Item key={index} className={scrollItem}>
-            {user.email}
-          </Flex.Item>
-        ))}
+          hasMoreData,
+          isLoading
+        }) => (
+          <div>
+            {data.map((user, index) => {
+              return (
+                <Flex.Item key={index} className={scrollItem}>
+                  <div style={{ marginBottom: "5px" }}>
+                    <Header bold h2 left skin={HeaderSkin}>
+                      {user.name.first} {user.name.last}
+                    </Header>
+                    <Header h3 left skin={HeaderSkin}>
+                      {user.email}
+                    </Header>
+                  </div>
+                  <img
+                    alt={user.name.first}
+                    src={user.picture.medium}
+                    style={{
+                      borderRadius: '50%',
+                      height: 72,
+                      marginRight: 20,
+                      width: 72,
+                    }}
+                  />
+                </Flex.Item>
+              );
+            })}
+            {!hasMoreData && <div>End of Users</div>}
+            {isLoading && <div>Loading..</div>}
+            {error && <div>Error</div>}
+          </div>
+        )}
       />
-    </div>
+    </Flex>
   ));
