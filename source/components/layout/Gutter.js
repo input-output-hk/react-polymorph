ca6e9e9c2dc45c36d8be17422309a5464b51216d
MarcusHurney <marcushurney@gmail.com>
2018-07-25 18:38:09 -0700

    [DDW-197] Connects Gutter to context for access to theme

--- source/components/layout/Gutter.js
+++ source/components/layout/Gutter.js
@@ -1,32 +1,70 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 
 // components
 import { Base } from './Base';
+import { withTheme } from '../HOC/withTheme';
 
-// styles
-import gutterStyles from '../../themes/helpers/Gutter.scss';
-
-// utilities
+// utility functions
+import { composeTheme, addThemeId } from '../../utils/themes';
 import { numberToPx } from '../../utils/props';
+
+// constants
+import { IDENTIFIERS } from '../../themes/API';
 
 type Props = {
   className: string,
-  padding: string | number
+  context: {
+    theme: Object,
+    ROOT_THEME_API: Object
+  },
+  padding: string | number,
+  theme: Object,
+  themeId: string,
+  themeOverrides: Object
 };
 
-export const Gutter = (props: Props) => {
-  const { children, className } = props;
-  const padding = props.padding ? numberToPx(props.padding) : null;
+type State = { composedTheme: Object };
 
-  return (
-    <Base
-      activeClasses={['gutter']}
-      className={className}
-      inlineStyles={{ padding }}
-      stylesToAdd={gutterStyles}
-    >
-      {children}
-    </Base>
-  );
-};
+class GutterBase extends Component<Props, State> {
+  // define static properties
+  static displayName = 'Gutter';
+  static defaultProps = {
+    theme: null,
+    themeId: IDENTIFIERS.GUTTER,
+    themeOverrides: {}
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    const { context, themeId, theme, themeOverrides } = props;
+
+    this.state = {
+      composedTheme: composeTheme(
+        addThemeId(theme || context.theme, themeId),
+        addThemeId(themeOverrides, themeId),
+        context.ROOT_THEME_API
+      )
+    };
+  }
+
+  render() {
+    const { children, className, themeId, padding: inlinePadding } = this.props;
+    const padding = inlinePadding ? numberToPx(inlinePadding) : null;
+    const theme = this.state.composedTheme[themeId];
+
+    return (
+      <Base
+        activeClasses={['gutter']}
+        className={className}
+        inlineStyles={{ padding }}
+        stylesToAdd={theme}
+      >
+        {children}
+      </Base>
+    );
+  }
+}
+
+export const Gutter = withTheme(GutterBase);
