ba88a01051b1936d9b59d137ac588d707fb2f42f
MarcusHurney <marcushurney@gmail.com>
2018-07-25 19:34:17 -0700

    [DDW-197] Improves data fetching and render methods within the InfiniteScroll story

--- stories/InfiniteScroll.stories.js
+++ stories/InfiniteScroll.stories.js
@@ -9,62 +9,60 @@ import { InfiniteScroll } from '../source/components/InfiniteScroll';
 import { Flex } from '../source/components/layout/Flex';
 import { Header } from '../source/components/Header';
 import { HeaderSkin } from '../source/skins/simple/HeaderSkin';
-import { Grid } from '../source/components/layout/Grid';
-import { Gutter } from '../source/components/layout/Gutter';
 
-// styles
+// styles && theme overrides
 import styles from './InfiniteScroll.stories.scss';
+import themeOverrides from './theme-overrides/customInfiniteScroll.scss';
 
-const { full, scrollItem } = styles;
+// helper classes
+const { full, itemsRoot } = styles;
 
 storiesOf('InfiniteScroll', module)
   // ====== InfiniteScroll Stories ======
   .add('simple', () => (
     <Flex center className={full}>
       <InfiniteScroll
+        themeOverrides={themeOverrides}
         fetchData={setState => {
-          fetch('https://randomuser.me/api/?results=12')
-          .then(res => res.json())
-          .then(({ results: nextUsers }) => setState({ isLoading: true }, () =>
-            setState(prevState => ({
-              error: false,
-              isLoading: false,
-              data: [...prevState.data, ...nextUsers]
-            }))
-          ))
-          .catch(error => setState({ error }));
+          setState({ isLoading: true }, async () => {
+            try {
+              const res = await fetch('https://randomuser.me/api/?results=12');
+              const { results: nextUsers } = await res.json();
+
+              setState(({ data: prevUsers }) => ({
+                error: false,
+                isLoading: false,
+                data: [...prevUsers, ...nextUsers]
+              }));
+            } catch (error) {
+              setState({ error, isLoading: false });
+            }
+          });
         }}
         renderItems={({
           data,
           error,
           hasMoreData,
-          isLoading
+          isLoading,
+          theme
         }) => (
-          <div>
-            {data.map((user, index) => {
-              return (
-                <Flex.Item key={index} className={scrollItem}>
-                  <div style={{ marginBottom: "5px" }}>
-                    <Header bold h2 left skin={HeaderSkin}>
-                      {user.name.first} {user.name.last}
-                    </Header>
-                    <Header h3 left skin={HeaderSkin}>
-                      {user.email}
-                    </Header>
-                  </div>
-                  <img
-                    alt={user.name.first}
-                    src={user.picture.medium}
-                    style={{
-                      borderRadius: '50%',
-                      height: 72,
-                      marginRight: 20,
-                      width: 72,
-                    }}
-                  />
-                </Flex.Item>
-              );
-            })}
+          <div className={itemsRoot}>
+            {data.map((user, index) => (
+              <Flex.Item key={index} theme={theme}>
+                <div style={{ marginBottom: '5px' }}>
+                  <Header bold h2 left skin={HeaderSkin}>
+                    {user.name.first} {user.name.last}
+                  </Header>
+                  <Header h3 left skin={HeaderSkin}>
+                    {user.email}
+                  </Header>
+                </div>
+                <img
+                  alt={user.name.first}
+                  src={user.picture.medium}
+                />
+              </Flex.Item>
+            ))}
             {!hasMoreData && <div>End of Users</div>}
             {isLoading && <div>Loading..</div>}
             {error && <div>Error</div>}
