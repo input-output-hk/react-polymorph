b833e026f4c84f6d9134bb97c711fd0615c1330e
MarcusHurney <marcushurney@gmail.com>
2018-07-25 18:36:08 -0700

    [DDW-197] Connects Grid to context for access to theme

--- source/components/layout/Grid.js
+++ source/components/layout/Grid.js
@@ -5,13 +5,15 @@ import { pickBy, isEmpty } from 'lodash';
 // components
 import { Base } from './Base';
 import { GridItem } from './GridItem';
-
-// styles
-import gridStyles from '../../themes/helpers/Grid.scss';
+import { withTheme } from '../HOC/withTheme';
 
 // utilities
 import { numberToPx } from '../../utils/props';
 import { formatTemplateAreas } from '../../utils/layout';
+import { composeTheme, addThemeId } from '../../utils/themes';
+
+// constants
+import { IDENTIFIERS } from '../../themes/API';
 
 type Props = {
   alignItems: string,
@@ -21,19 +23,47 @@ type Props = {
   center: boolean,
   columnGap: string | number,
   columns: string,
+  context: {
+    theme: Object,
+    ROOT_THEME_API: Object
+  },
   gap: string | number,
   justifyItems: string,
   rowGap: string | number,
   rows: string,
   template: string,
-  templateAreas: Array<''>
+  templateAreas: Array<''>,
+  theme: Object,
+  themeId: string,
+  themeOverrides: Object
 };
 
-export class Grid extends Component<Props> {
+type State = { composedTheme: Object };
+
+class GridBase extends Component<Props, State> {
+  // define static properties
+  static displayName = 'Grid';
   static defaultProps = {
     columnGap: 5,
-    rowGap: 5
+    rowGap: 5,
+    theme: null,
+    themeId: IDENTIFIERS.GRID,
+    themeOverrides: {}
   };
+
+  constructor(props: Props) {
+    super(props);
+
+    const { context, themeId, theme, themeOverrides } = props;
+
+    this.state = {
+      composedTheme: composeTheme(
+        addThemeId(theme || context.theme, themeId),
+        addThemeId(themeOverrides, themeId),
+        context.ROOT_THEME_API
+      )
+    };
+  }
 
   // creates obj passed Base component's inline styles (see render)
   _assembleInlineGrid = () => {
@@ -76,21 +106,33 @@ export class Grid extends Component<Props> {
     return pickBy(inlineClasses);
   }
 
+  renderChildren(theme: Object) {
+    return React.Children.map(this.props.children, child => {
+      if (child.type.displayName === 'GridItem') {
+        return React.cloneElement(child, { theme });
+      }
+      return child;
+    });
+  }
+
   render() {
-    const { children, className, ...gridProps } = this.props;
+    const { themeId, className, ...gridProps } = this.props;
+
     const inlineGrid = this._assembleInlineGrid({ ...gridProps });
+    const theme = this.state.composedTheme[themeId];
 
     return (
       <Base
         className={className}
-        stylesToAdd={gridStyles}
-        activeClasses={['grid']}
+        stylesToAdd={theme}
+        activeClasses={['container']}
         inlineStyles={inlineGrid}
       >
-        {children}
+        {this.renderChildren(theme)}
       </Base>
     );
   }
 }
 
+export const Grid = withTheme(GridBase);
 Grid.Item = GridItem;
