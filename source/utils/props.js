32887e3cbb8a343a563284ddec49955c78ec58d3
MarcusHurney <marcushurney@gmail.com>
2018-07-30 22:40:34 -0700

    Adds getDisplayName utility function

--- source/utils/props.js
+++ source/utils/props.js
@@ -7,3 +7,6 @@ export const pickDOMProps = filterReactDomProps;
 
 export const composeFunctions = (...fns: [Function, Function]) => (...args: [any, any]) =>
   fns.forEach(fn => fn && fn(...args));
+
+export const getDisplayName = (Component: ComponentType<*>) =>
+  (Component.displayName || Component.name);
