4c4bae7c41ee98528a53bd1c8e9d97cebaf10717
MarcusHurney <marcushurney@gmail.com>
2020-01-14 22:55:45 -0500

    [DDW-829] Swaps className prop for style prop

--- source/skins/simple/ScrollBarSkin.js
+++ source/skins/simple/ScrollBarSkin.js
@@ -2,25 +2,25 @@
 import React from 'react';
 
 // external libraries
-import classnames from 'classnames';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 type Props = {
   children: Node,
   className: string,
+  style: Object,
   theme: Object,
   themeId: string
 };
 
 export const ScrollBarSkin = (props: Props) => {
-  const { children, className, themeId } = props;
+  const { children, style, themeId } = props;
   const theme = props.theme[themeId];
 
   const renderThumb = (thumbProps: any) => <div {...thumbProps} className={theme.thumb} />;
 
   return (
     <Scrollbars
-      className={classnames([className, theme.root])}
+      style={style}
       renderThumbVertical={renderThumb}
     >
       {children}
