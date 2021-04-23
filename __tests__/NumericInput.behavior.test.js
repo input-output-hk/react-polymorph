c3438c987e5d3bab5c7e7e71d275e6e3fca13bb6
Dominik Guzei <dominik.guzei@gmail.com>
2021-04-23 12:35:18 +0200

    [DDW-595] Fix failing tests

--- __tests__/NumericInput.behavior.test.js
+++ __tests__/NumericInput.behavior.test.js
@@ -23,8 +23,7 @@ describe('NumericInput onChange simulations', () => {
       const { input, onChangeMock } = mountNumericInputWithProps();
       input.simulate('change', { nativeEvent: { target: { value: '19.00' } } });
       const onChangeValue = onChangeMock.mock.calls[0][0];
-      expect(BigNumber.isBigNumber(onChangeValue)).toBe(true);
-      expect(onChangeValue.toNumber()).toBe(19);
+      expect(onChangeValue).toEqual('19');
     });
     test('invalid input does not trigger onChange listener', () => {
       const { input, onChangeMock } = mountNumericInputWithProps();
@@ -40,7 +39,7 @@ describe('NumericInput onChange simulations', () => {
         nativeEvent: { target: { value: '9,999,999.00' } },
       });
       const onChangeValue = onChangeMock.mock.calls[0][0];
-      expect(onChangeValue.toNumber()).toBe(9999999.0);
+      expect(onChangeValue).toBe('9999999');
     });
     test('can be configured to handle dots as thousand separators', () => {
       const { input, onChangeMock } = mountNumericInputWithProps({
@@ -52,7 +51,7 @@ describe('NumericInput onChange simulations', () => {
       input.simulate('change', {
         nativeEvent: { target: { value: '9.999.999,00' } },
       });
-      expect(onChangeMock.mock.calls[0][0].toNumber()).toBe(9999999);
+      expect(onChangeMock.mock.calls[0][0]).toBe('9999999');
     });
     test('can be configured to handle spaces as thousand separators', () => {
       const { input, onChangeMock } = mountNumericInputWithProps({
@@ -64,7 +63,7 @@ describe('NumericInput onChange simulations', () => {
       input.simulate('change', {
         nativeEvent: { target: { value: '9 999 999.00' } },
       });
-      expect(onChangeMock.mock.calls[0][0].toNumber()).toBe(9999999);
+      expect(onChangeMock.mock.calls[0][0]).toBe('9999999');
     });
   });
 });
